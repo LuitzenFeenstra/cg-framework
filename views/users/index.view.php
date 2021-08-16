7a6d7dc7438838a9e80de13be56315aa9346f78a
LuitzenFeenstra <luitzenfeenstra@live.nl>
2021-08-16 10:00:51 +0200

    user buttons

--- views/users/index.view.php
+++ views/users/index.view.php
@@ -3,9 +3,15 @@
     <div class="row">
         <?php foreach ($vars['users'] as $user) : ?>
             <div class="col-md-3"><?= $user->first_name ?></div>
-            <div class="col-md-8"><?= $user->last_name ?></div>
+            <div class="col-md-6"><?= $user->last_name ?></div>
             <div class="col-md-1">
                 <a class="btn" href="/user/<?= $user->id ?>">view</a>
+            </div>
+            <div class="col-md-1">
+                <a class="btn" href="/user/<?= $user->id ?>/edit">edit</a>
+            </div>
+            <div class="col-md-1">
+                <a class="btn" href="/user/<?= $user->id ?>/destroy">delete</a>
             </div>
         <?php endforeach ?>
         <div class="col-12">
